54cc7ea4325b3278b4df453576250bcf98136b01
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-12 11:03:22 -0400

    C# Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //C# Methods Exercise 1
+            
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What the initial of your last name?");
+            string lastInitial = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string favColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string favBand = Console.ReadLine();
+
+            Console.WriteLine("What city or country would you like to visit?");
+            string visitLocation = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite food?");
+            string favFood = Console.ReadLine();
+
+            Console.WriteLine($"Hi {userName} {lastInitial}. Thank you for visiting our travel site and telling us about yourself.");
+            Console.WriteLine($"We have prepared a wonderful package for you to visit {visitLocation}.");
+            Console.WriteLine($"This package will include airfare in a private {favColor} airplane, hotel,");
+            Console.WriteLine($"and front row tickets to a performance by {favBand}.");
+            Console.WriteLine($"{favBand} will be accompanied by a giant dancing {favAnimal}.");
+            Console.WriteLine($"We are also including all your meals at an all you can eat {favFood} bar");
+            
+
         }
     }
 }
